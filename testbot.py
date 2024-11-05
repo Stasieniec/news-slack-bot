b16a1e7212fef889f42812219cac2f42e739f70b
Stanisław Wasilewski <wasilewski.sf@gmail.com>
2024-11-05 12:20:05 +0100

    test date

--- testbot.py
+++ testbot.py
@@ -8,7 +8,7 @@ client = WebClient(token=apis.SLACK_TOKEN)
 
 # Function to send a message to a specified Slack channel
 def send_message_to_slack():
-    message = main_function(from_date='2024-10-010', to_date='2024-10-20')  # Call the test function and get its output
+    message = main_function(from_date='2024-10-10', to_date='2024-10-20')  # Call the test function and get its output
     try:
         # Send the message to the specified channel
         response = client.chat_postMessage(
